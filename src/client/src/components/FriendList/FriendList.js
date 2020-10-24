1fe596aa9ffb1ba11c3d8f5257d0669106ce1a67
Jérémy <jeremyschneider21@gmail.com>
2020-10-24 17:12:10 +0200

    Add sliding animation

--- src/client/src/components/FriendList/FriendList.js
+++ src/client/src/components/FriendList/FriendList.js
@@ -3,22 +3,29 @@ import {Col, Media, Navbar} from 'react-bootstrap';
 import 'holderjs';
 
 import Header from '../Header/Header';
+import './FriendList.scss';
 
 const FriendList = () => {
-    const handleNavbar = (e) => {
-     
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleNavbar = () => {
+        if (isOpen) {
+            setIsOpen(false);
+        } else {
+            setIsOpen(true);
+        }
     };
 
     return (
-        <Col md="10" xs="12" className="friend-list h-100 bg-dark">
+        <Col md="10" xs="12" className={`friend-list h-100 bg-dark ${isOpen ? 'open' : ''}`}>
             <Navbar bg="dark" variant="dark" className="justify-content-between align-items-center">
                 <Navbar.Brand className="d-none d-sm-block">Friends</Navbar.Brand>
-                <div className="mobile-menu d-block d-sm-none">
+                <div className="mobile-menu d-block d-sm-none" onClick={handleNavbar}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             d="M4 6h16M4 12h16M4 18h16"
                         />
                     </svg>
