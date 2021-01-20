367ffc46c3acc7cd3a9904ba2e7522b7b46220e6
Jérémy <jeremyschneider21@gmail.com>
2021-01-20 16:29:13 +0100

    Add swipe animation to close the nav

--- src/client/src/components/FriendList/FriendList.js
+++ src/client/src/components/FriendList/FriendList.js
@@ -6,6 +6,7 @@ import Cookies from 'js-cookie';
 import Card from '../Card/Card';
 import AddFriend from '../AddFriend/AddFriend';
 import FriendListReducer from './FriendListReducer';
+import {useSwipeable} from 'react-swipeable';
 
 import './FriendList.scss';
 
@@ -56,41 +57,45 @@ const FriendList = () => {
         getFriends();
     }, []);
 
+    const handlers = useSwipeable({
+        onSwipedLeft: () => {
+            dispatch({type: 'close'});
+        },
+    });
+
     return (
-        <Fragment>
-            <Col md="10" xs="12" className={`friend-list h-100 bg-dark ${state.isOpen ? 'open' : ''}`}>
-                <Navbar bg="dark" variant="dark" className="justify-content-between align-items-center">
-                    <Navbar.Brand className="d-none d-sm-block">Friends</Navbar.Brand>
-                    <Nav variant="pills" defaultActiveKey="friends" onSelect={handleSelect}>
-                        <Nav.Item>
-                            <Nav.Link eventKey="friends">Friends</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="pending">Pending</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="blocked">Blocked</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="add_friend">Add Friend</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                    <div className="mobile-menu d-block d-sm-none" onClick={handleNavbar}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h16M4 18h16"
-                            />
-                        </svg>
-                    </div>
-                </Navbar>
-                <ul className="friends-list list-unstyled">
-                    {state.pillsFriend == 'add_friend' ? <AddFriend /> : friends}
-                </ul>
-            </Col>
-        </Fragment>
+        <Col md="10" xs="12" className={`friend-list h-100 bg-dark ${state.isOpen ? 'open' : ''}`} {...handlers}>
+            <Navbar bg="dark" variant="dark" className="justify-content-between align-items-center">
+                <Navbar.Brand className="d-none d-sm-block">Friends</Navbar.Brand>
+                <Nav variant="pills" defaultActiveKey="friends" onSelect={handleSelect}>
+                    <Nav.Item>
+                        <Nav.Link eventKey="friends">Friends</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="pending">Pending</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="blocked">Blocked</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="add_friend">Add Friend</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <div className="mobile-menu d-block d-sm-none" onClick={handleNavbar}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4 6h16M4 12h16M4 18h16"
+                        />
+                    </svg>
+                </div>
+            </Navbar>
+            <ul className="friends-list list-unstyled">
+                {state.pillsFriend == 'add_friend' ? <AddFriend /> : friends}
+            </ul>
+        </Col>
     );
 };
 
--- src/client/src/components/Main/Main.js
+++ src/client/src/components/Main/Main.js
@@ -1,6 +1,6 @@
 import React, {Fragment} from 'react';
 import {Redirect, Switch, Route} from 'react-router-dom';
-import {Container, Row} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
 
 import PrivateRoute from '../CustomRoute/PrivateRoute';
 
