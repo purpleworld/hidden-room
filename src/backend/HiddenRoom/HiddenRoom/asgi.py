45b39c3698f66e52e908f5dcdd3bd8f9d5fe438b
Jérémy <jeremyschneider21@gmail.com>
2021-04-24 20:23:35 +0200

    Update asgi.py

--- src/backend/HiddenRoom/HiddenRoom/asgi.py
+++ src/backend/HiddenRoom/HiddenRoom/asgi.py
@@ -1,4 +1,5 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -7,7 +8,7 @@ from .TokenAuthMiddleware import TokenAuthMiddleware
 import Chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HiddenRoom.settings')
-
+django.setup()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": TokenAuthMiddleware(
