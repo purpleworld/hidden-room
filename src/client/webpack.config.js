a829c46dc83a3903a8148afcad42ce860d1a4fc5
Jérémy <jeremyschneider21@gmail.com>
2021-05-04 00:18:21 +0200

    Update webpack config
    Add html-webpack-plugin and mini-css-extract-plugin

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
 const path = require('path');
 const dotenv = require('dotenv').config({path: path.join(__dirname, '.env')});
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: {
@@ -25,13 +27,23 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                loader: ['style-loader', 'css-loader', 'sass-loader'],
+                include: [path.resolve(__dirname, 'src/sass')],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             },
         ],
     },
     plugins: [
         new webpack.DefinePlugin({
             'process.env': JSON.stringify(dotenv.parsed),
+        }),
+        new HtmlWebpackPlugin({
+            template: __dirname + '/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+            chunkFilename: '[id].css',
         }),
     ],
 };
